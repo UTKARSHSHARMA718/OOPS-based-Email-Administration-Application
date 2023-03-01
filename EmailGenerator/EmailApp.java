aa7e8f73aff97876c286876d4b80711fa2ac8e3f
UTKARSH SHARMA <86897568+UTKARSHSHARMA718@users.noreply.github.com>
2023-03-01 13:06:20 +0530

    Update EmailApp.java

--- EmailGenerator/EmailApp.java
+++ EmailGenerator/EmailApp.java
@@ -1,8 +1,15 @@
 package EmailGenerator;
+import java.util.Scanner;
 
 public class EmailApp {
     public static void main(String[] args) {
-        Email create = new Email("King","Sharma");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please Enter your First name: ");
+        String fn = sc.next();
+        System.out.print("Please Enter your Last name: ");
+        String ln = sc.next();
+        System.out.println();
+        Email create = new Email(fn,ln);
 
     }
 }
